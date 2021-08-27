927e0d176bd11402da074da350268d4807613df3
ssavl <get-moscow@yandex.ru>
2021-08-27 10:22:27 +0300

    добавил конфиг в package-json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./'));
+app.use(express.static('./dist'));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
